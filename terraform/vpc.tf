b2b2c1def55a6a2d580cb2d6d56e49c27ea00b72
isseikitazawa <i.kitazawa@nana-music.com>
2023-03-12 16:12:15 +0900

    VPC作成

---
+++ terraform/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "issei-prd-vpc" {
+  assign_generated_ipv6_cidr_block = false
+  cidr_block                       = "10.0.0.0/16"
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  instance_tenancy                 = "default"
+  tags = {
+    "Name" = "issei-prd-vpc"
+    "env"  = "prd"
+  }
+}
